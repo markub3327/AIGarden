46a4b4b9644a3d98a43df6e23f83c21b9715e6f7
markub <markub3327@gmail.com>
2021-07-24 14:42:09 +0200

    update main.py

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # AIGarden
 
+[![Release](https://img.shields.io/github/release/markub3327/AIGarden)](https://github.com/markub3327/AIGarden/releases)
+![Tag](https://img.shields.io/github/v/tag/markub3327/AIGarden)
+[![Issues](https://img.shields.io/github/issues/markub3327/AIGarden)](https://github.com/markub3327/AIGarden/issues)
+![Commits](https://img.shields.io/github/commit-activity/w/markub3327/AIGarden)
+![Languages](https://img.shields.io/github/languages/count/markub3327/AIGarden)
+![Size](https://img.shields.io/github/repo-size/markub3327/AIGarden)
+
 Automate your garden with machine learning and Raspberry Pi.
+
+----------------------------------
+
+**Frameworks:** Tensorflow, OpenCV
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import os
 
 # Watering
 def watering(motor):
     for force in range(1,101,1):
         motor.ChangeDutyCycle(force)
-        sleep(0.05)
+        sleep(0.5)
+        print(f"Watering ... {force} %")
     sleep(5)
     motor.ChangeDutyCycle(0)
 
+# Init console
+clear = lambda: os.system('clear')
+
+# Init GPIO
 GPIO.setmode(GPIO.BOARD)
 
 # Pumps
 motor1_pin = 32
 motor2_pin = 33
-
 GPIO.setup(motor1_pin, GPIO.OUT)
 GPIO.setup(motor2_pin, GPIO.OUT)
-
 motor1_pwm = GPIO.PWM(motor1_pin, 1000)     # 1kHz
 motor1_pwm.start(0)
-
 motor2_pwm = GPIO.PWM(motor2_pin, 1000)     # 1kHz
 motor2_pwm.start(0)
 
+# Main loop
 while True:
-    print("AIGarden")
-    print("")
+    print("AIGarden 🚰🥕🍅")
+    print("Bc. Martin Kubovčík")
+    print("https://github.com/markub3327/AIGarden")
+    print()
     print("        1  Watering")
     print("        2  Scan")
+    print("        3  Scan")
+    print("        q  Quit")
     print()
     cmd = input(">> ")
 
@@ -37,5 +45,19 @@ while True:
         watering(motor2_pwm)
     elif cmd == "2":
         print("Scanning ...")
+    elif cmd == "3":
+        end = False
+        while end == False:
+            print("Settings")
+            print("        q  Quit")
+            print()
+            cmd = input(">> ")
+
+            if cmd == 'q':
+                break
+            else:
+                print("Bad command was eneterd.")
     else:
         print("Bad command was eneterd.")
+    
+    clear()     # clear console
